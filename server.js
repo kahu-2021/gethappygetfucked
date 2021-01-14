3aca2b7d9ea7e34f692c1d69f3c1034fef9395d9
student <student@eda-bc-06.localdomain>
2021-01-15 12:44:06 +1300

    Simple pages.

--- server.js
+++ server.js
@@ -4,17 +4,42 @@ const hbs = require('express-handlebars')
 const server = express()
 
 const fs = require('fs')
-const router = require('./routes')
+//const router = require('./routes')
 
 // Server configuration
 server.use(express.static('public'))
-server.use(express.urlencoded({ extended: false }))
+server.use(express.urlencoded({ extended: true }))
 
 // Handlebars configuration
 server.engine('hbs', hbs({ extname: 'hbs' }))
 server.set('view engine', 'hbs')
 
 // Your routes/router(s) should go here
+
+server.get('/', (req, res) => {
+  res.render('./home')
+})
+
+server.post('/questions', (req, res) => {
+  let name = {name : req.body.name}
+  res.render('./questions', name)
+  //res.send(name)
+})
+
+server.get('/questions', (req, res) => {
+  res.send("questions")
+})
+
+server.post('/compliment', (req, res) => {
+  res.send("hi you look great")
+})
+
+server.post('/insult', (req, res) => {
+  res.send("insult!")
+})
+
+
+
 
 
 module.exports = server
